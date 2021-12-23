165ac53a822098d2ca6aa574909d5252f05e8adb
thorsush <thoratsushil703.com>
2021-12-23 11:14:43 +0530

    updated with the project

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import  './App.css';
+import Calcibox from './Calcibox';
+
+
+export default function App() {
+  return (
+    <div>
+     <Calcibox/>
+    </div>
+  )
+}
+
+
+ 
