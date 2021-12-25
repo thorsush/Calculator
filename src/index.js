ddbe45c42ccab09fba7c593a4646120f4c0d0b1b
thorsush <thoratsushil703.com>
2021-12-25 11:11:11 +0530

    last try

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter /*Route*/} from "react-router-dom";
 
 
 ReactDOM.render(
   <React.StrictMode>
-   <HashRouter base="/">
-        <App />
-    </HashRouter>
+          <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
